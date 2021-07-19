3e16d8560a229100c5d4e50065e23fa11664c4b6
jpcorvalan <76654210+jpcorvalan@users.noreply.github.com>
2021-07-19 11:24:55 -0300

    clase Reserva
    Se agrega la clase Reserva, que mapea una tabla en la base de datos. Esta tabla será la encargada de guardar los datos de las reservas una vez estén concretadas.

---
+++ src/java/Logica/Reserva.java
@@ -0,0 +1,155 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ * Clase que mapea la reserva de la habitación si se cumplen la condiciones necesarias
+ *
+ * @author Corvalán Juan Pablo
+ */
+@Entity
+@Table(name = "reservas")
+public class Reserva implements Serializable {
+    
+    // Variables de instancia
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+    
+    @Temporal(TemporalType.DATE)
+    @Column(name = "check_in")
+    private Calendar checkIn;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "check_out")
+    private Calendar checkOut;
+    
+    @Basic
+    @Column(name = "cantidad_personas")
+    private int cantPersonas;
+    
+    @OneToOne
+    @Column(name = "habitacion")
+    private Habitacion habitacion;
+    
+    @OneToOne
+    @Column(name = "huesped")
+    private Persona huesped;
+    
+    @OneToOne
+    @Column(name = "usuario_alta")
+    private Persona usuarioAlta;
+    
+    @Basic
+    @Column(name = "estado")
+    private boolean estaActiva;
+    
+    
+    
+    
+    // Constructores
+    public Reserva() {
+    }
+
+    public Reserva(int id, Calendar checkIn, Calendar checkOut, int cantPersonas, Habitacion habitacion, Persona huesped, Persona usuarioAlta, boolean estaActiva) {
+        this.id = id;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+        this.cantPersonas = cantPersonas;
+        this.habitacion = habitacion;
+        this.huesped = huesped;
+        this.usuarioAlta = usuarioAlta;
+        this.estaActiva = estaActiva;
+    }
+    
+    
+    
+    
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCheckIn(Calendar checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public void setCheckOut(Calendar checkOut) {
+        this.checkOut = checkOut;
+    }
+
+    public void setCantPersonas(int cantPersonas) {
+        this.cantPersonas = cantPersonas;
+    }
+
+    public void setHabitacion(Habitacion habitacion) {
+        this.habitacion = habitacion;
+    }
+
+    public void setHuesped(Persona huesped) {
+        this.huesped = huesped;
+    }
+
+    public void setUsuarioAlta(Persona usuarioAlta) {
+        this.usuarioAlta = usuarioAlta;
+    }
+
+    public void setEstaActiva(boolean estaActiva) {
+        this.estaActiva = estaActiva;
+    }
+    
+    
+    
+    
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public Calendar getCheckIn() {
+        return checkIn;
+    }
+
+    public Calendar getCheckOut() {
+        return checkOut;
+    }
+
+    public int getCantPersonas() {
+        return cantPersonas;
+    }
+
+    public Habitacion getHabitacion() {
+        return habitacion;
+    }
+
+    public Persona getHuesped() {
+        return huesped;
+    }
+
+    public Persona getUsuarioAlta() {
+        return usuarioAlta;
+    }
+
+    public boolean isEstaActiva() {
+        return estaActiva;
+    }
+    
+    
+}
